91985f0e1bb897232108f4cb3d7d90c8109c042f
Aminu Ndakun <amin4712@gmail.com>
2023-05-11 11:50:26 +0100

    adding dynamo db function

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const winston = require('winston');
 const cors = require('cors');
+const AWS = require('aws-sdk'),
+      {
+        DynamoDBDocument
+      } = require("@aws-sdk/lib-dynamodb"),
+      {
+        DynamoDB
+      } = require("@aws-sdk/client-dynamodb");
+const uuid = require('uuid');
+require('dotenv').config();
 const app = express();
 
 // define the logger
@@ -21,13 +30,6 @@ const logger = winston.createLogger({
 // use the logger to log events
 logger.info('Server running on port 3000');
 
-// log an error
-try {
-  throw new Error('Oops!');
-} catch (error) {
-  logger.error(error.message); // log the error message
-}
-
 app.use(cors());
 
 // parse requests of content-type - application/x-www-form-urlencoded
@@ -36,12 +38,42 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 
+// configure the AWS SDK
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION
+});
+
+// create a new instance of the DocumentClient
+const documentClient = DynamoDBDocument.from(new DynamoDB());
+
 // define a route to receive data from the Android app
 app.post('/', (req, res) => {
   const { watTime, deviceName, latitude, longitude } = req.body;
   // process the data
   console.log(`Received location data: ${watTime}, ${deviceName} (${latitude}, ${longitude})`);
-  res.status(200).send('Data received successfully');
+
+  // create a new item in the DynamoDB table
+  const params = {
+    TableName: 'your-table-name',
+    Item: {
+      id: uuid.v1(),
+      watTime,
+      deviceName,
+      latitude,
+      longitude
+    }
+  };
+  documentClient.put(params, (err, data) => {
+    if (err) {
+      console.error(`Unable to add item. Error JSON: ${JSON.stringify(err, null, 2)}`);
+      res.status(500).send('Failed to store location data');
+    } else {
+      console.log(`Location data stored in DynamoDB: ${JSON.stringify(params.Item)}`);
+      res.status(200).send('Data received and stored successfully');
+    }
+  });
 });
 
 // start the server
